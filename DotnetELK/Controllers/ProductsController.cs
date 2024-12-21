7aad81d45bb84fea15e71a63c9fe08ff450a2071
elmurod <elmuroddevvv@gmail.com>
2024-12-21 20:41:50 +0500

    Added All Project Files

---
+++ DotnetELK/Controllers/ProductsController.cs
@@ -0,0 +1,84 @@
+﻿using DotnetELK.Models;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace DotnetELK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //MAQSADI   
+        /*ProductsController sinfi quyidagilarni amalga oshiradi:
+         1. GET so'rovi orqali mahsulotlarni qidirish.
+         2. POST so'rovi orqali yangi mahsulot qo'shish.*/
+
+        #region Constructor
+        private readonly IElasticClient _elasticClient;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IElasticClient elasticClient, ILogger<ProductsController> logger)
+        {
+            _elasticClient = elasticClient;
+            _logger = logger;
+        }
+        // _elasticClient Elasticsearch bilan bog'lanish uchun ishlatiladi.
+        // _logger: Log yozuvlarini boshqarish uchun ishlatiladi.
+        #endregion
+
+        #region Get Products
+        //Vazifasi:
+        /*Kiritilgan kalit so'zga mos keladigan 
+         mahsulotlarni Elasticsearch indeksidan qidiradi.*/
+        [HttpGet(Name = "Get Products")]
+        public async Task<IActionResult> Get(string keyword)
+        {
+            try
+            {
+                var results = await _elasticClient.SearchAsync<Product>(
+                    s => s.Query(
+                        q => q.QueryString(
+                            d => d.Query('*' + keyword + '*')
+                            )
+                        ).Size(100)
+                    );
+                //SearchAsync: Elasticsearch'dagi Product indeksida qidiruvni amalga oshiradi.
+                //s => s.Query(...): Qidiruv so'rovini aniqlaydi.
+                //q => q.QueryString(...): Kalit so'z asosida matn qidiruvini amalga oshiradi.
+                /*('*' + keyword + '*'): * belgisi kalit so'zdan oldin va keyin qo'shilib, 
+                 to'liq mos kelmagan (partial match) qidiruvni amalga oshiradi.*/
+                // .Size(1000): Maksimal 1000 ta natija qaytaradi.
+                return Ok(results.Documents.ToList());
+                //results.Documents.ToList(): Elasticsearch dan olingan mahsulotlarni ro'yxat sifatida qaytaradi.
+                //Ok(...): HTTP 200 javob holati bilan natijani qaytaradi.
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        #endregion
+
+        #region Add Product
+        // Vazifasi:
+        //Yangi mahsulotni Elasticsearch indeksiga qo'shadi.
+        [HttpPost(Name = "AddProduct")]
+        public async Task<IActionResult> Post(Product product)
+        {
+            try
+            {
+                await _elasticClient.IndexDocumentAsync(product);
+                /*IndexDocumentAsync: Kiritilgan product obyektini Elasticsearch'ga 
+                 indekslaydi (yangi yozuv qo'shadi).*/
+                return Created("Product Created!", product);
+                //Ok(): Agar mahsulot muvaffaqiyatli qo'shilsa, HTTP 200 holatini qaytaradi.
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+        #endregion
+    }
+}
---
+++ DotnetELK/Extensions/ElasticSearchExtensions.cs
@@ -0,0 +1,89 @@
+﻿using DotnetELK.Models;
+using Nest;
+using System.Runtime.CompilerServices;
+
+namespace DotnetELK.Extensions
+{
+    public static class ElasticSearchExtensions
+    {
+        #region About
+        //Sinfning MAQSADI
+        /*ElasticSearchExtensions sinfi quyidagilarni amalga oshiradi:
+          1. Elasticsearch mijozini sozlash va xizmatlar konteyneriga qo'shish.
+          2. Model uchun standart xaritalash qoidalarini belgilash.
+          3. Elasticsearch indeksini avtomatik yaratish.*/
+
+        //METHODLAR
+        //Vazifasi
+        /*Elasticsearch ulanish sozlamalarini o'qish.
+         Elasticsearch mijozini (ElasticClient) yaratish.
+        Xizmatlar konteyneriga (IServiceCollection) Elasticsearch mijozini qo'shish.
+        Standart indeksni yaratish.*/
+
+        //PEARAMETRLAR
+        //IServiceCollection services: Xizmatlar konteyneri.
+        //IConfiguration configuration: Konfiguratsiya.
+        #endregion
+
+        #region AddElasticSearch
+        //QANADAY ISHLAYDI
+        public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Elasticsearch ulanish sozlamalari.
+            var url = configuration["ELKConfiguration:Uri"];
+            var defaultIndex = configuration["ELKConfiguration:index"];
+            //URI - Elasticsearch serverining URL manzili.
+            //index - Standart indeks nomi.
+
+            //Elasticsearch sozlamalarini yaratish.
+            var settings = new ConnectionSettings(new Uri(url))
+                .PrettyJson()
+                .DefaultIndex(defaultIndex);
+            //PrettyJson() - JSON so'zlamalarini chiroyli formatda qaytaradi.
+            //DefaultIndex(defaultIndex) - Standart indeksni belgilaydi.
+
+            //Standart xaritalarni qo'shish.
+            AddDefaultMappings(settings);
+            /*Bu metod Product modelining ayrim xususiyatlarini (masalan, Price va Quantity) 
+             indekslashdan chiqarib tashlaydi.*/
+
+            //Elasticsearch mijozini yaratish.
+            var client = new ElasticClient(settings);
+            services.AddSingleton<IElasticClient>(client);
+            //ElasticClient → Elasticsearch bilan ishlash uchun mijoz yaratadi.
+            /*services.AddSingleton() →
+            Mijozni ilova davomida bir marta yaratib, butun ilovada qayta ishlatadi.*/
+
+            //Standart indeksni yaratish.
+            CreateIndex(client, defaultIndex);
+            //Bu metod standart indeksni yaratadi.
+        }
+        #endregion
+
+        #region AddDefaultMappings
+        //Vazifasi
+        //Modelning ayrim xususiyatlarini indeksdan chiqarib tashlaydi.
+        private static void AddDefaultMappings(ConnectionSettings settings)
+        {
+            //DefaultMappingFor<Product> yordamida Product modeliga xaritalash qoidalari qo'llanadi.
+            /*Ignore() → Ko'rsatilgan xususiyatlar (Price va Quantity) Elasticsearch indeksida saqlanmaydi.*/
+            settings.DefaultMappingFor<Product>(p =>
+            p.Ignore(x => x.Price)
+            .Ignore(x => x.Quantity));
+        }
+        #endregion
+
+        #region CreateIndex
+        //Vazifasi
+        //Elasticsearch'da indeks yaratish.
+        private static void CreateIndex(IElasticClient client, string indexName)
+        {
+            client.Indices.Create(indexName, i => i.Map<Product>(x => x.AutoMap()));
+            //client.Indices.Create() → Yangi indeksni yaratadi.
+            /*i.Map<Product>(x => x.AutoMap()) → Product modelining
+            barcha xususiyatlarini avtomatik xaritalashni amalga oshiradi (AutoMap).*/
+        }
+        #endregion
+    }
+
+}
